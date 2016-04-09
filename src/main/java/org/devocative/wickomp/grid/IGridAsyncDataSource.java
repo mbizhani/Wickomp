ccce1feb7bc257f08ff1b62e919416f5e69f24b4
Mehdi Bizhani <mbizhani@gmail.com>
2016-04-09 21:10:11 +0430

    a minor change in javadoc

--- src/main/java/org/devocative/wickomp/grid/IGridAsyncDataSource.java
+++ src/main/java/org/devocative/wickomp/grid/IGridAsyncDataSource.java
@@ -9,7 +9,6 @@ public interface IGridAsyncDataSource<T> extends IDataSource<T> {
 	 * @param pageIndex  page number during pagination process starts with 1
 	 * @param pageSize   size of a page during pagination process
 	 * @param sortFields list of fields for sorting the result (optional)
-	 * @return list of result
 	 */
 	void list(long pageIndex, long pageSize, List<WSortField> sortFields);
 }
