abdd06ec205d4b31ad760c499859b8662e37de0c
Mehdi Bizhani <mbizhani@gmail.com>
2016-04-24 14:32:56 +0430

    dropdown-popup implementation of  WClientSearchableListInput

--- src/main/java/org/devocative/wickomp/form/WClientSearchableListInput.java
+++ src/main/java/org/devocative/wickomp/form/WClientSearchableListInput.java
@@ -1,16 +1,20 @@
 package org.devocative.wickomp.form;
 
+import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.Component;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.AjaxLink;
 import org.apache.wicket.markup.head.HeaderItem;
 import org.apache.wicket.markup.head.IHeaderResponse;
+import org.apache.wicket.markup.html.WebComponent;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.model.IModel;
 import org.devocative.wickomp.WFormInputPanel;
+import org.devocative.wickomp.WebUtil;
 import org.devocative.wickomp.html.window.OModalWindow;
 import org.devocative.wickomp.html.window.WModalWindow;
+import org.devocative.wickomp.wrcs.CommonBehavior;
 import org.devocative.wickomp.wrcs.FontAwesomeBehavior;
 import org.devocative.wickomp.wrcs.Resource;
 
@@ -18,11 +22,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public abstract class WClientSearchableListInput<T> extends WFormInputPanel<List<T>> {
+	private static final HeaderItem SEL_LIST_JS = Resource.getCommonJS("form/selList/selList.js");
+	private static final HeaderItem SEL_LIST_CSS = Resource.getCommonJS("form/selList/selList.css");
 	private static final HeaderItem JS = Resource.getCommonJS("form/clientSearchList/clientSearchList.js");
 
 	private Label label;
 	private WModalWindow modalWindow;
 	private WebMarkupContainer result;
+	private WebComponent title;
+
+	// ---------------------------- CONSTRUCTORS
 
 	public WClientSearchableListInput(String id) {
 		this(id, null);
@@ -41,6 +50,9 @@ public abstract class WClientSearchableListInput<T> extends WFormInputPanel<List
 		result.setOutputMarkupId(true);
 		add(result);
 
+		add(title = new WebComponent("title"));
+		title.setOutputMarkupId(true);
+
 		add(new AjaxLink("openModal") {
 			@Override
 			public void onClick(AjaxRequestTarget target) {
@@ -50,8 +62,13 @@ public abstract class WClientSearchableListInput<T> extends WFormInputPanel<List
 			}
 		});
 
+		setOutputMarkupId(true);
+
 		add(new FontAwesomeBehavior());
+		add(new CommonBehavior());
 	}
+
+	// ---------------------------- ACCESSORS
 
 	public OModalWindow getModalWindowOptions() {
 		return modalWindow.getOptions();
@@ -62,23 +79,22 @@ public abstract class WClientSearchableListInput<T> extends WFormInputPanel<List
 		return this;
 	}
 
+	// ---------------------------- ABSTRACT METHODS
+
+	protected abstract Component createSelectionPanel(String selectionPanelId);
+
+	protected abstract T createServerObject(String key);
+
+	// ---------------------------- PUBLIC METHODS
+
 	@Override
 	public void renderHead(IHeaderResponse response) {
 		Resource.addJQueryReference(response);
 
+		response.render(SEL_LIST_CSS);
+		response.render(SEL_LIST_JS);
+
 		response.render(JS);
-	}
-
-	@Override
-	protected void onInitialize() {
-		super.onInitialize();
-
-		IModel<String> labelModel = getLabel();
-		if (labelModel != null) {
-			label.setDefaultModel(labelModel);
-		} else {
-			label.setVisible(false);
-		}
 	}
 
 	@Override
@@ -98,14 +114,41 @@ public abstract class WClientSearchableListInput<T> extends WFormInputPanel<List
 		}
 	}
 
-	protected abstract Component createSelectionPanel(String selectionPanelId);
+	// ---------------------------- PROTECTED METHODS
 
-	protected abstract T createServerObject(String key);
+	@Override
+	protected void onInitialize() {
+		super.onInitialize();
+
+		IModel<String> labelModel = getLabel();
+		if (labelModel != null) {
+			label.setDefaultModel(labelModel);
+		} else {
+			label.setVisible(false);
+		}
+	}
+
+	@Override
+	protected void onBeforeRender() {
+		title.add(new AttributeModifier("value", "0"));
+
+		super.onBeforeRender();
+	}
+
+	@Override
+	protected void onAfterRender() {
+		super.onAfterRender();
+
+		String script = String.format("initClientSearchableList('%s');", getMarkupId());
+
+		WebUtil.writeJQueryCall(script, false);
+	}
 
 	protected String getJSCallback() {
-		return String.format("function(rows){handleClientSearchableList('%s', '%s', '%s', rows);}",
+		return String.format("function(rows){handleClientSearchableList('%s', '%s', '%s', '%s', rows);}",
 			modalWindow.getContainerMarkupId(),
 			getInputName(),
-			result.getMarkupId());
+			result.getMarkupId(),
+			title.getMarkupId());
 	}
 }
--- src/main/java/org/devocative/wickomp/wrcs/form/clientSearchList/clientSearchList.js
+++ src/main/java/org/devocative/wickomp/wrcs/form/clientSearchList/clientSearchList.js
@@ -1,20 +1,52 @@
-function handleClientSearchableList(modalWindowId, inputName, holderTableId, rows) {
+function initClientSearchableList(selListPanelId) {
+	var selListPanel = $("#" + selListPanelId);
+	var slOpener = selListPanel.find(".slOpener:first");
+	var slTitle = selListPanel.find(".slTitle:first");
+	var slDropDown = selListPanel.find(".slDropDown:first");
+
+	slTitle.focusin(function (event) {
+		closeHandler_SelList(event);
+		slDropDown.css("display", "inline");
+		slDropDown.css("visibility", "visible");
+		lastOpenedSelList = slDropDown;
+		preventEvent(event)
+	});
+
+	slDropDown.bind('focusin mouseup', function (event) {
+		preventEvent(event);
+	});
+
+	slOpener.click(function (event) {
+		closeHandler_SelList(event);
+		slDropDown.css("display", "inline");
+		slDropDown.css("visibility", "visible");
+		lastOpenedSelList = slDropDown;
+	});
+
+	//overwrite the CSS 120px value
+	slTitle.css("width", "85px");
+}
+
+function handleClientSearchableList(modalWindowId, inputName, holderTableId, titleId, rows) {
 	if (rows) {
 		var holder = $('#' + holderTableId);
+		var title = $('#' + titleId);
 		holder.empty();
 		for (var r = 0; r < rows.length; r++) {
 			var input = $('<input type="hidden" name="' + inputName + '" value="' + rows[r]["key"] + '"/>');
 			var span = $('<span>' + rows[r]["value"] + '</span>');
 
-			var td1 = $('<td></td>');
+			var td1 = $('<td style="border-bottom: 2px solid #cccccc"></td>');
 			td1.append(input);
 			td1.append(span);
 
 			var delAct = $('<i class="fa fa-times" style="color:red;cursor:pointer;"></i>');
 			delAct.bind("click", function () {
 				$(this).parentsUntil("tr").parent().remove();
+				var count = title.val();
+				title.val(count - 1);
 			});
-			var td2 = $('<td></td>');
+			var td2 = $('<td style="border-bottom: 2px solid #cccccc"></td>');
 			td2.append(delAct);
 
 			var tr = $('<tr></tr>');
@@ -23,6 +55,7 @@ function handleClientSearchableList(modalWindowId, inputName, holderTableId, row
 
 			holder.append(tr);
 		}
+		title.val(rows.length);
 	}
 	$('#' + modalWindowId).window('close');
 }
